3eebbb1448eeb183be970bfed74a391bc478eedb
Jayasri1702 <110477471+Jayasri1702@users.noreply.github.com>
2024-03-02 08:38:27 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/chat_appj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_appj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/chat.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+import 'package:chat_appj/widgets/chat_messages.dart';
+import 'package:chat_appj/widgets/new_message.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterChat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: const [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessage(),
+        ],
+      ),
+    );
+  }
+}
